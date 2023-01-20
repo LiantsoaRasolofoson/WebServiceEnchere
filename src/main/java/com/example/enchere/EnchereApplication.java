de284328397f2e6b9dae51500ee32f3995fdb0ce
NasoloEricka <anjaranasoloericka@gmail.com>
2023-01-20 18:38:18 +0300

    push

--- src/main/java/com/example/enchere/EnchereApplication.java
+++ src/main/java/com/example/enchere/EnchereApplication.java
@@ -2,11 +2,30 @@ package com.example.enchere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class EnchereApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnchereApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
 	}
 }
